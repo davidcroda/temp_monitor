79eca7fddcdc808475d2a573aed53d73f39c206c
David Roda <davidcroda@gmail.com>
2023-02-28 16:19:25 -0500

    Remove deprecated non live views

--- lib/temp_monitor_web/controllers/page_controller.ex
+++ lib/temp_monitor_web/controllers/page_controller.ex
@@ -6,10 +6,4 @@ defmodule TempMonitorWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
-
-  def graph(conn, _params) do
-    temperature = Data.get_latest_temperature()
-    accounts = Alerts.list_accounts()
-    render(conn, "graph.html", %{temperature: temperature, accounts: accounts})
-  end
 end
